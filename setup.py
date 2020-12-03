6dd3a905d2e9c9a69b3037420b8b423d80a5db36
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:06:35 +0300

    2020:12:04 00:06

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-display',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/display','scripts/display-sleep','scripts/display-state','scripts/display-wake']
